8fbb84d29bf3f4f90919782894f7e2b2aa031154
nitis <nitish8152@gmail.com>
2022-10-10 11:17:37 +0530

    Added: Create Simple Linked List

--- src/com/bridgelab/LinkedList.java
+++ src/com/bridgelab/LinkedList.java
@@ -1,7 +1,42 @@
 package com.bridgelab;
 
-public class LinkedList {
-    public static void main(String[] args) {
-        System.out.println("Welcome To Linked List Problem");
+public class LinkedList <T>{
+    Node<T> head;
+    Node<T> tail;
+
+    public void push(T data) {
+        Node<T> newNode = new Node(data);
+        if(head == null){
+            head = newNode;
+            tail = newNode;
+        }else{
+            newNode.next = head;
+            head = newNode;
+        }
+    }
+
+    public void add(T data) {
+        Node<T> newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        }else{
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
+    public void show(){
+        if(head == null){
+            System.out.println("Linked list is Empty");
+        }
+        else{
+            Node<T> temp = head;
+            while(temp != null){
+                System.out.print(temp.data + " ");
+                temp = temp.next;
+            }
+            System.out.println();
+        }
+
     }
 }
---
+++ src/com/bridgelab/LinkedListMain.java
@@ -0,0 +1,13 @@
+package com.bridgelab;
+
+public class LinkedListMain {
+    public static void main(String[] args) {
+        LinkedList<Integer> list1 = new LinkedList();
+        list1.push(70);
+        list1.push(30);
+        list1.push(56);
+        list1.show();
+
+        System.out.println();
+    }
+}
